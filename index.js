fb9b82143fdd5ae72cfdca969e1ed4a87b88a381
purple <victoria.ruf@etu.unicaen.fr>
2025-06-06 05:46:41 +0200

    Ajout gestion des inputs ( erreur + correction )

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ const formulaire = document.getElementById("formulaireMeteo");
 const codePostal = document.getElementById("codePostal"); // On entre le code postal
 const selectionVille = document.getElementById("selectionVille"); // On selectionne la ville correspondante
 const resultat = document.getElementById("resultat"); // Zone d'affichage des résultats
+
+codePostal.addEventListener("input", () => {
+  // Suppression des espaces
+  const codeP = codePostal.value.trim();
+  // Suppression des anciennes villes
+  selectionVille.innerHTML = "";
+  // Première gestion des errreurs : il n'y a pas 5 chiffres 
+  if(codeP.lenght !== 5) return;
+
+  
+})
